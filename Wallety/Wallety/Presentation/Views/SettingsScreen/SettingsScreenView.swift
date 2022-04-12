41200dfc852d77faa4a02d13f1c3f0747e76e171
ankonovalov <anatoliyk@wizardsdev.com>
2022-04-12 21:37:55 +0300

    Add settings screen and settings cell

---
+++ Wallety/Wallety/Presentation/Views/SettingsScreen/SettingsScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsScreenView.swift
+//  Wallety
+//
+//  Created by Anatoliy Konovalov on 12.04.2022.
+//
+
+import UIKit
+
+class SettingsScreenView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
