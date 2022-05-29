a876846e70e7887e2e1dec8d30b878670ed569da
ankonovalov <anatoliyk@wizardsdev.com>
2022-05-29 22:17:38 +0300

    Some changes
    - Add noData view
    - Replace empty stack with view
    - Add separator to settings and font size
    - Some refactoring

---
+++ Wallety/Wallety/Presentation/Views/EmptyState/NoDataEmptyView.swift
@@ -0,0 +1,73 @@
+import UIKit
+
+final class NoDataEmptyView: UIView {
+    
+    // MARK: - Properties
+    
+    override var isHidden: Bool {
+        get {
+            return emptyStack.isHidden
+        }
+        set {
+            emptyStack.isHidden = newValue
+        }
+    }
+    
+    // MARK: - Views
+    
+    lazy var emptyStack: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel])
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.distribution = .fill
+        return view
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let view = UILabel(text: R.string.localizable.noOperations(),
+                           font: UIFont(name: "KohinoorGujarati-Regular", size: 26)!,
+                           textColor: .white)
+        view.textAlignment = .center
+        return view
+    }()
+    
+    lazy var descriptionLabel: UILabel = {
+        let view = UILabel(text: R.string.localizable.forThisPeriod(),
+                           font: UIFont(name: "KohinoorGujarati-Regular", size: 14)!,
+                           textColor: .white.withAlphaComponent(0.5))
+        view.textAlignment = .center
+        return view
+    }()
+    
+    // MARK: - Lifecycle
+    
+    init(title: String, description: String) {
+        super.init(frame: .zero)
+        manageView(title: title, description: description)
+        addSubviews()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private
+    
+    private func manageView(title: String, description: String) {
+        self.backgroundColor = R.color.primaryBackground()
+        titleLabel.text = title
+        descriptionLabel.text = description
+    }
+    
+    private func addSubviews() {
+        self.addSubview(emptyStack)
+    }
+    
+    private func addConstraints() {
+        emptyStack.snp.makeConstraints { make in
+            make.centerX.centerY.equalTo(self)
+        }
+    }
+    
+}
